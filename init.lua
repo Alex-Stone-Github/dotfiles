15ce061a9d06a66961ec3a8d354593740f98e4d2
Alex Stone <stonealex145@gmail.com>
2022-09-10 19:36:07 -0500

    Luaification

---
+++ init.lua
@@ -0,0 +1,89 @@
+--
+-- Config
+--
+local g = vim.g
+local o = vim.o
+
+-- sane defaults
+o.backup = false
+o.writebackup = false
+o.undofile = true
+o.swapfile = false
+o.timeoutlen = 500
+o.updatetime = 200
+o.wrap = false;
+o.clipboard = 'unnamedplus'
+o.history = 50
+
+-- nice colors / gui
+o.termguicolors = true
+o.number = true
+o.relativenumber = true
+o.signcolumn = 'yes'
+o.cursorline = true
+
+-- Tabs dun dun dun
+o.expandtab = true
+o.smarttab = true
+o.cindent = true
+o.tabstop = 4
+o.shiftwidth = 0
+o.softtabstop = -1
+
+-- Searching
+o.ignorecase = true
+o.smartcase = true
+
+-- color scheme
+vim.cmd("colo darcula")
+
+
+--
+-- Keybindings
+--
+
+-- mode key value
+local function map(m, k, v)
+	vim.keymap.set(m, k, v, { silent = true })
+end
+
+-- split navigation
+map('n', '<S-h>', "<C-w>h")
+map('n', '<S-j>', "<C-w>j")
+map('n', '<S-k>', "<C-w>k")
+map('n', '<S-l>', "<C-w>l")
+
+-- file navigation
+map('n', '<S-m>', ":Telescope find_files prompt_prefix=🔍<CR>")
+
+-- terminal navigation
+map('n', '<S-t>', ":term<CR>")
+
+--
+-- Plugins
+--
+local vim = vim
+local execute = vim.api.nvim_command
+local fn = vim.fn
+-- ensure that packer is installed
+local install_path = fn.stdpath('data')..'/site/pack/packer/opt/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+    execute('!git clone https://github.com/wbthomason/packer.nvim '..install_path)
+    execute 'packadd packer.nvim'
+end
+vim.cmd('packadd packer.nvim')
+local packer = require'packer'
+local util = require'packer.util'
+packer.init({
+  package_root = util.join_paths(vim.fn.stdpath('data'), 'site', 'pack')
+})
+--- startup and add configure plugins
+packer.startup(function()
+    local use = use
+    -- add you plugins here like:
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.0',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
+    end
+)
