9c7f0b5ede004bcf2c34f4149c17fecb8d0d34b9
Alex <stonealex145@gmail.com>
2024-09-10 08:24:35 -0500

    Quick Compile Fix

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,12 +1,19 @@
-
+-- ---------------
+-- Basic Config --
+-- ---------------
 vim.opt.number = true
 vim.opt.mouse = 'a'
+vim.cmd [[colorscheme gruvbox]]
+
+-- Gotta love tabs
 
 -- ------------------
 -- Plugins & Stuff --
 -- ------------------
 vim.cmd [[packadd packer.nvim]]
-
 return require('packer').startup(function(use)
+	use {
+		"nvim-telescope/telescope.nvim", tag= '0.1.4', requires = { {'nvim-lua/plenary.nvim'} }
+	}
 	use "morhetz/gruvbox"
 end)
