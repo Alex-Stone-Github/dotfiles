be547d744c0bcd0e1a6d0b422f09d58e3385dbfb
Alex Stone <stonealex145@gmail.com>
2025-06-23 08:43:30 -0500

    Lazy Nvim fix

--- nvim/init.lua
+++ nvim/init.lua
@@ -27,20 +27,21 @@ mapit("n", "<s-l>", "<c-w>l");
 -- -----
 -- Plugins and stuff
 -- -----
--- install lazyvim if not already installed
+-- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-	local outpath = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-	if vim.v.shell_error ~= 0 then
-		vim.api.nvim_echo({
-			{"Failed to clone lazy.nvim:\n", "ErrorMsg" },
-			{out, "WarningMsg" },
-			{"\nPress any key to exit..." },
-		}, true, {})
-		vim.fn.getchar()
-		os.exit(1)
-	end
+  print("Insalling lazy nvim")
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
 vim.opt.rtp:prepend(lazypath)
 
